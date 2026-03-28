4392781a3514742a5bcd9df89b718a49ff3f8f51
krysof <krysof@users.noreply.github.com>
2026-03-28 16:25:21 +0900

    fix: headbutt tilt axis perpendicular to facing direction, works all directions v20260328.30

--- js/physics.js
+++ js/physics.js
@@ -403,13 +403,15 @@ function updateEggPhysics(egg, isCity){if(egg.heldBy||egg._piledriverLocked)retu
         var _hdT=egg._hondaDashTotal||60;
         var _hdP=_hdT-egg._hondaDash;
         var _hdA=0;
-        if(_hdP<8){_hdA=(_hdP/8)*1.57;}
-        else if(egg._hondaDash>5){_hdA=1.57;}
-        else{var _l2=(5-egg._hondaDash)/5;_hdA=egg._hondaBounced?-1.57*(1-_l2):1.57*(1-_l2);}
+        if(_hdP<8){_hdA=-(_hdP/8)*1.57;}
+        else if(egg._hondaDash>5){_hdA=-1.57;}
+        else{var _l2=(5-egg._hondaDash)/5;_hdA=egg._hondaBounced?1.57*(1-_l2):-1.57*(1-_l2);}
+        // Tilt axis = perpendicular to facing direction (local right)
         var _hdF=Math.atan2(egg._dashDirX,egg._dashDirZ);
+        var _tiltAxis=new THREE.Vector3(-Math.cos(_hdF),0,Math.sin(_hdF)).normalize();
         var _q1=new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0,1,0),_hdF);
-        var _q2=new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1,0,0),_hdA);
-        egg.mesh.quaternion.copy(_q1.multiply(_q2));
+        var _q2=new THREE.Quaternion().setFromAxisAngle(_tiltAxis,_hdA);
+        egg.mesh.quaternion.copy(_q2).multiply(_q1);
     }
 }
 
